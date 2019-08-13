c1c9c7750a1a3d1c3adde191407964a955e42529
liwenguang <liwenguang_dev@163.com>
2019-08-13 20:35:08 +0800

    第二章

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,27 @@ fn main() {
     println!("The secret number is: {}", secret_number);
 
     loop {
-    println!("Plese input your guess.");
+        println!("Plese input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
 
-    io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
 
-    let guess: u32 = match  guess.trim().parse()
-    .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {
-            println!("You win!");
-            break;
-            },
-    }
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+                },
+        }
     }
 
 }
