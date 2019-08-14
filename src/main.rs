41f32078139ccef077e9dea97c9e45f7b2fc857c
liwenguang <liwenguang_dev@163.com>
2019-08-14 20:58:55 +0800

    crate mod

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,55 @@
+mod sound {
+    pub mod instrument {
+        pub fn clarinet() {
+            super::breathe_in();
+        }
+    }
+
+    fn breathe_in() {}
+}
+
+mod plant {
+    pub struct Vegetable {
+        pub name: String,
+        id: i32,
+    }
+
+    impl Vegetable {
+        pub fn new(name: &str)->Vegetable{
+            Vegetable{
+                name: String::from(name),
+                id: 1,
+            }
+        }
+    }
+}
+
+mod menu {
+    // 公共枚举属性都为公共
+    pub enum Appetizer{
+        Soup,
+        Salad,
+    }
+}
+// 绝对路径引入
+use crate::sound::instrument;
+// 相对路径引入
+use self::sound::instrument;
+
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
-    let loopback = IpAddr::V6(String::from("::1"));
+    crate::sound::instrument::clarinet();
 
-    let some_number = Some(5);
-    let some_string = Some("a string");
+    sound::instrument::clarinet();
 
-    value_in_cents(Coin::Quarter(UsState::Alaska));
-    value_in_cents(Coin::Dime);
+    let mut v = plant::Vegetable::new("squash");
 
-    let some_u8_value = 0u8;
-    match some_u8_value {
-        1=>println!("one"),
-        3=>println!("three"),
-        _=>(),
-    }
+    v.name = String::from("butternut squash");
 
-    let mut count = 0;
-    if let Coin::Quarter(state) = coin {
-        println!("State quarter from {:?}!", state);
-    } else {
-        count += 1;
-    }
-}
+    println!("{} are delicious", v.name);
+    // println!("{}", v.id);
 
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        },
-    }
-}
+    let order1 = menu::Appetizer::Soup;
+    let order2 = menu::Appetizer::Salad;
 
-enum IpAddr {
-    V4(String),
-    V6(String),
-}
-
-enum Option<T> {
-    Some(T),
-    None,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-#[derive(Debug)]
-enum UsState{
-    Alabama,
-    Alaska,
+    instrument::clarinet();
 }
