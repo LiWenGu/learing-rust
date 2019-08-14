c5d7a38e386010794f3a294de9a552b938020dc2
liwenguang <liwenguang_dev@163.com>
2019-08-15 00:15:22 +0800

    String 底层使用 vector. hashmap key value 类型固定

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,43 @@
-mod sound {
-    pub mod instrument {
-        pub fn clarinet() {
-            super::breathe_in();
-        }
-    }
-
-    fn breathe_in() {}
-}
-
-mod plant {
-    pub struct Vegetable {
-        pub name: String,
-        id: i32,
-    }
-
-    impl Vegetable {
-        pub fn new(name: &str)->Vegetable{
-            Vegetable{
-                name: String::from(name),
-                id: 1,
-            }
-        }
-    }
-}
-
-mod menu {
-    // 公共枚举属性都为公共
-    pub enum Appetizer{
-        Soup,
-        Salad,
-    }
-}
-// 绝对路径引入
-use crate::sound::instrument;
-// 相对路径引入
-use self::sound::instrument;
+use std::collections::HashMap;
 
 fn main() {
-    crate::sound::instrument::clarinet();
+    let v: Vec<i32> = Vec::new();
 
-    sound::instrument::clarinet();
+    let v= vec![1,2,3,4];
+    let third: &i32 = &v[2];
 
-    let mut v = plant::Vegetable::new("squash");
+    println!("The third element is {}", third);
 
-    v.name = String::from("butternut squash");
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    };
 
-    println!("{} are delicious", v.name);
-    // println!("{}", v.id);
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 
-    let order1 = menu::Appetizer::Soup;
-    let order2 = menu::Appetizer::Salad;
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
 
-    instrument::clarinet();
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let mut scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    for (key, value) in &scores {
+        println!("{}, {}", key, value);
+    }
+    let key = String::from("Yello");
+    scores.entry(&key).or_insert(&50);
+}
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
 }
