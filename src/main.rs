b3eab77af55a037aeba753dc97488cac06251a72
liwenguang <liwenguang_dev@163.com>
2019-08-14 18:04:52 +0800

    关联函数？和静态方法

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 fn main() {
     let rect1 = Rectangle {width: 30, height: 50};
 
-    println!("rect1 is {:?}", area(&rect1));
+    println!("rect1 is {:?}", rect1.area());
+    println!("rect1 is {:?}", Rectangle::square(5));
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    // 关联函数？静态函数？
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
 }
+
 
 #[derive(Debug)]
 struct Rectangle {
