ae13a2b4ba44ef55743d1548ae4820af78746c05
liwenguang <liwenguang_dev@163.com>
2019-08-14 17:49:04 +0800

    结构体

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,20 @@
 fn main() {
-    let tup = (500, 6.4, 1);
-
-    let (x, y, z) = tup;
-
-    println!("The value of y is: {}, {}, {}", x, y, z);
-
-    let a = [1, 2, 3, 4, 5];
-
-    println!("{}", a[1]);
-
-    let condition = true;
-
-    let number = if condition {
-        5
-    } else {
-        6
-    };
-
-    println!("{}", number);
-
-
-    let s1 = String::from("hello");
-    let s2 = s1;
-    println!("{}", s2);
-
-    let s = String::from("hello");  // s 进入作用域
-
-    takes_ownership(s);             // s 的值移动到函数里 ...
-    // ... 所以到这里不再有效
-
-    let x = 5;                      // x 进入作用域
-
-    makes_copy(x);                  // x 应该移动函数里，
-
+    let u = build_user(String::from("sad"));
+    println!("{}", u.email);
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+fn build_user(email: String) -> User {
+    User {
+        email,
+        username: String::from("asdasd"),
+        active: true,
+        sign_in_count: 1
+    }
 }
