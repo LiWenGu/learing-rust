ab43af6cdffd99dd5cde3faab132e09b8a103b3d
liwenguang <liwenguang_dev@163.com>
2019-08-15 16:05:02 +0800

    生命周期

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,75 @@
-use std::fs::File;
-use std::io::{ErrorKind, Read};
-use std::io;
+use std::fmt::{Display, Debug};
 
 fn main() {
-    let f = File::open("hello.txt").map_err(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Tried to create file but there was a problem: {:?}", error);
-            })
-        } else {
-            panic!("There was a problem opeing the file: {:?}", error);
-        }
-    });
-    // not used
-    f.ok();
+    let number_list = vec![34, 50, 25, 100];
+    let result = largest(&number_list);
+    println!("{}", result);
+    let number_list = vec![102, 42, 6000];
+    let result = largest(&number_list);
+    println!("{}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    println!("T{}", result);
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+
+    println!("p3{},{}", p3.x, p3.y);
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years age...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt{part : first_sentence};
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
 
-    let mut f = match f {
-        OK(file) => file,
-        Err(e) => return Err(e),
-    };
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 
-    let mut s = String::new();
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
 
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
-fn read_username_from_file2() -> Result<String, io::Error> {
-    let mut f = File::open("hello.txt")?;
+fn some_function<T, U>(t: T, u: U) -> i32
+    where T: Display + Clone,
+          U: Clone + Debug
+{
+    2
+}
 
-    let mut s = String::new();
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
 
-    f.read_to_string(&mut s)?;
-    Ok(s)
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
